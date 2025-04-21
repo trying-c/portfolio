caafb47d534fca69d99b43447dfcbceeb7ccfdde
trying <tryingchung@qq.com>
2025-04-21 15:04:18 +0800

    feat: github action to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [vue()],
-    base: '/portfolio/', // 设置打包路径
+    base: './', // 设置打包路径
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
